f46c54310a901d97b28f136e49a2fabd75dc4a68
Hoàng Nguyên <71932200+SleepyCatOnTheBed@users.noreply.github.com>
2021-12-02 21:49:31 +0700

    Update Chapter1-a.py

--- Hints/Chapter1-a.py
+++ Hints/Chapter1-a.py
@@ -1,5 +1,31 @@
+#a)
 def linearSearch(list,item):
     for i in list:
         if i==item:
             return f"Found the number at index {listOfNumber.index(i)}"
     return "Number not in the list"
+#b) Theory
+
+#c) Simply count the number of comparisions
+#For example:
+def linearSearch(list,item):
+    let step = 1;
+    for i in list:
+        if i==item:
+            print("Number of steps: ",step)
+            return f"Found the number at index {listOfNumber.index(i)}"
+        step++;
+    return "Number not in the list"
+
+#In question b
+let low...
+let high...
+let step = 1;
+...
+if i == item:
+    print("Number of steps: ",step)
+    return f"Found the number at index {listOfNumber.index(i)}"
+...
+step++
+
+#Done
